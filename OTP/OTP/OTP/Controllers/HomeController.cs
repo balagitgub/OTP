0c971f6237b62fd83a6465f6883d69f44d831963
Balasubramani R <balasubramani.career@gmail.com>
2020-07-23 05:27:08 +0000

    commit2

---
+++ OTP/OTP/OTP/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OTP.Models;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+namespace OTP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(model person)
+        {
+            Random r = new Random();
+            string OTP = r.Next(1000, 9999).ToString();
+            string PH = person.Phonenumber;
+            const string accountSid = "ACcddb1ab2fd24e62badd680a6f1aec966";
+            const string authToken = "d80221e9202d321bcd3fbcbe8ceab64c";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "Your OTP is: " +OTP,
+                from: new Twilio.Types.PhoneNumber("+12029337942"),
+                to: new Twilio.Types.PhoneNumber("+91"+ PH + "")
+            );
+
+            
+
+            
+            Session["OTP"] = OTP;
+
+            //Redirect for varification
+           
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ OTP/OTP/OTP/Models/model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OTP.Models
+{
+    public class model
+    {
+        
+            public int PersonId { get; set; }
+
+            
+            public string Name { get; set; }
+
+            
+            public string Gender { get; set; }
+
+
+            public string Phonenumber { get; set; }
+
+
+        public string City { get; set; }
+       
+    }
+}
